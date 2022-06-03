fbeb72ff86a2a1d1b6e5dc786c93b2b44693639c
Sergey Kuznetsov <mr.enline@gmail.com>
2022-06-03 17:50:02 +0300

    release

--- authController.js
+++ authController.js
@@ -1,16 +1,57 @@
+const Role = require('./models/Role.js');
+const User = require('./models/User.js');
+const bcrypt = require('bcryptjs');
+const { validationResult } = require('express-validation');
+
 class authController {
     async registration(req, res) {
         try {
-        } catch (error) {}
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res
+                    .status(400)
+                    .json({ message: 'Error on registration' });
+            }
+            const { username, password } = req.body; //получаем имя и пароль из тела запроса
+            const candidate = User.findOne(username);
+            if (candidate) {
+                return res.status(400).json({
+                    message: 'A user with the same name already exists',
+                });
+            }
+            const hashPassword = bcrypt.hashSync(password, 7);
+            const userRole = await Role.findOne({ value: 'USER' });
+            const user = new User({
+                username,
+                password: hashPassword,
+                roles: [userRole.value],
+            });
+            await user.save();
+            return res.json({
+                message: 'Пользователь успешно зарегистрирован',
+            });
+        } catch (error) {
+            console.log(error);
+            res.status(400).json({ message: 'Registration error' });
+        }
     }
 
     async login(req, res) {
         try {
-        } catch (error) {}
+        } catch (error) {
+            console.log(error);
+            res.status(400).json({ message: 'Login error' });
+        }
     }
 
     async getUsers(req, res) {
         try {
+            //добавление ролей в БД. Таким путем лучше не делать никогда
+            // const userRole = new Role();
+            // const adminRole = new Role({ value: 'ADMIN' });
+            // await userRole.save();
+            // await adminRole.save();
+            res.json('Server working');
         } catch (error) {}
     }
 }
--- authRouter.js
+++ authRouter.js
@@ -1,10 +1,22 @@
 const Router = require('express');
 const authController = require('./authController');
 const router = Router();
+const { check } = require('express-validator');
+
 //данный роутер прослушивает маршруты, отмеченные ниже
-//
-router.post('/registration', authController.registration());
-router.post('/login', authController.login());
-router.get('/users', authController.getUsers());
+//и выполняет методы, которые задаются вторым параметром
+router.post(
+    '/registration',
+    [
+        check('username', 'Field cannot be empty').notEmpty(),
+        check(
+            'password',
+            'Password must be more than 4 and less than 12 characters'
+        ).isLength(4, 12),
+    ],
+    authController.registration
+);
+router.post('/login', authController.login);
+router.get('/users', authController.getUsers);
 
 module.exports = router;
